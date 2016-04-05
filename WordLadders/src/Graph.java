58c1a6a15454569ecd5e98df3695cca0ab633b53
Mats Granholm <elt12mgr@student.lth.se>
2016-04-05 11:43:34 +0200

    Implementations on Graph class

--- WordLadders/src/Graph.java
+++ WordLadders/src/Graph.java
@@ -1,4 +1,54 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Graph {
+	private List<List<Integer>> nodes;
+	private int size;
+	
+	public Graph(int size) {
+		this.size = size;
+		nodes = new ArrayList<List<Integer>>();
+		for (int i = 0 ; i < size; ++i) {
+			nodes.add(new ArrayList<Integer>());
+		}
+		
+		nodes.get(0).add(1);
+		nodes.get(0).add(2);
+		nodes.get(2).add(3);
+		nodes.get(3).add(2);
+	}
+	
+	public ArrayList<Integer> getNeighbours(int node) {
+		ArrayList<Integer> ret = new ArrayList<Integer>();
+		for (Integer i : nodes.get(node)) {
+			ret.add(i);
+		}
+		return ret;
+	}
+	
+	public int bfs(int start, int end) {
+		boolean[] discovered = new boolean[size];
+		int i = 0;
+		List<List<Integer>> L = new ArrayList<List<Integer>>();
 
+		L.add(new ArrayList<Integer>());
+		L.get(0).add(start);
+		discovered[start] = true;
+		
+		while (!(L.get(i).isEmpty())) {
+			L.add(new ArrayList<Integer>());
+			for (Integer u : L.get(i)) {
+				ArrayList<Integer> neighbours = getNeighbours(u);
+				for (Integer v : neighbours) {
+					if (!discovered[v]) {
+						if (v == end) return i+1;
+						discovered[v] = true;
+						L.get(i + 1).add(v);
+					}
+				}
+			}
+			++i;
+		}
+		return -1;
+	}
 }
--- WordLadders/src/WordLadders.java
+++ WordLadders/src/WordLadders.java
@@ -1,8 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class WordLadders {
 
 	public static void main(String[] args) {
+		int n = 4;
 		
+		/*String[] nodeNames = new String[n];
+		nodeNames[0] = "other";
+		nodeNames[1] = "there";
+		nodeNames[2] = "their";
+		nodeNames[3] = "write";*/
+		Map<String, Integer> nodeNames = new HashMap<String, Integer>(n);
+		nodeNames.put("other", 0);
+		nodeNames.put("there", 1);
+		nodeNames.put("their", 2);
+		nodeNames.put("write", 3);
+		
+		Graph graph = new Graph(n);
+		
+		System.out.println(graph.bfs(nodeNames.get("other"), nodeNames.get("their")));
 	}
 
 }
